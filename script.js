588cbb2fffa9e09e9e98f2fa415b587a35f83a44
kongby08 <kongby96@gmail.com>
2026-01-14 12:09:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function openModal() {
+  document.getElementById("orderModal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("orderModal").style.display = "none";
+}
+
+function payNow() {
+     alert("这里可以付款");
+  closeModal();
+}
+
+function contactSeller() {
+ const message = encodeURIComponent(
+    "你好，我想订购【多功能保温杯】"
+  );
+
+  // 改成卖家的 WhatsApp 号码
+  const phone = "01128208828";
+
+  window.open(
+    `https://wa.me/${phone}?text=${message}`,
+    "_blank"
+  );
+}
+
+
